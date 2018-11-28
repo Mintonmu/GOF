b05b9a3fb70cb8ef069aaf6ad3647e3fd23815e9
muming <2859595067@qq.com>
2018-11-28 19:09:31 +0800

    单例模式的学习

--- src/Singlecase/Client.java
+++ src/Singlecase/Client.java
@@ -1,5 +1,8 @@
 package Singlecase;
 
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author:muming
  * @date:2018/11/28 11:24
@@ -7,9 +10,28 @@ package Singlecase;
  */
 public class Client {
 
-    public static void main(String[] args) {
-        SinglecaseDemo02 s = SinglecaseDemo02.getInstance();
-        SinglecaseDemo02 ss = SinglecaseDemo02.getInstance();
-        System.out.println(s == ss);
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException, IOException {
+        SinglecaseDemo06 s = SinglecaseDemo06.getInstance();
+        SinglecaseDemo06 ss = SinglecaseDemo06.getInstance();
+//反射
+//        Class<SinglecaseDemo06> clazz = (Class<SinglecaseDemo06>) Class.forName("Singlecase.SinglecaseDemo06");
+//
+//        Constructor<SinglecaseDemo06> constructor = clazz.getDeclaredConstructor(null);
+//        constructor.setAccessible(true);
+//
+//        SinglecaseDemo06 s2 = clazz.newInstance();
+//        SinglecaseDemo06 s3 = clazz.newInstance();
+//反序列化
+        FileOutputStream fileOutputStream = new FileOutputStream("./log.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(s);
+        objectOutputStream.close();
+        fileOutputStream.close();
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("./log.txt"));
+        SinglecaseDemo06 sw = (SinglecaseDemo06) objectInputStream.readObject();
+        System.out.println(sw);
+        System.out.println(s);
+        System.out.println(ss);
+
     }
 }
